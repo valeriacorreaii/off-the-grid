c59fc7549bf055d8f7674b9df3bddaa07881eb39
valeriacorreaii <valeriacorrea54@gmail.com>
2026-03-25 19:03:49 +0000

    java

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
 // main.js
 
-console.log("Hello, World!");
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min) + 1) + min;
+}
+let divs = document.querySelectorAll(".grid-container div");
+
+function randomize(){
+    divs.forEach(function(div){
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
+        div.style.transform =
+            'scale(${scale})
+            translate(${translateX}%, ${translateY}%)
+            rotate(${rotate}deg)';
+    })
+    console.log(divs.length, "randomize!" )
+}
