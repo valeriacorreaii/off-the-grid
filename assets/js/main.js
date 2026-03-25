9db1f2866ad39e59b6b803df4c5ef6c0ccd4ec8c
valeriacorreaii <valeriacorrea54@gmail.com>
2026-03-25 19:13:15 +0000

    java correct

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,31 @@
 // main.js
 
-
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (max - min) + 1) + min;
-}
+let grid = document.querySelectorAll(".grid-container");
 let divs = document.querySelectorAll(".grid-container div");
 
-function randomize(){
-    divs.forEach(function(div){
+function randomize() {
+    console.log(divs.length, "randomize!")
+
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`;
+
+       divs.forEach(function(div){
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0, 50);
         let translateY = randomNumber(0, 20);
         let rotate = randomNumber(0, 360);
-        div.style.transform =
-            'scale(${scale})
-            translate(${translateX}%, ${translateY}%)
-            rotate(${rotate}deg)';
-    })
-    console.log(divs.length, "randomize!" )
+
+        if (Math.random() > .5) {
+            scale = 1; // don't affect scale
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
+        }
+
+         div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
 }
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
